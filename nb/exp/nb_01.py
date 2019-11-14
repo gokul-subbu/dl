4d2db152c3bf3026631984d8dfc53ae0ceb8945a
narain1 <narainp@outlook.com>
2019-11-14 20:04:50 +0530

    created categorical notebook

--- nb/exp/nb_01.py
+++ nb/exp/nb_01.py
@@ -6,45 +6,473 @@
 
 import operator
 
-def test(a, b, cmp, cname=None):
-    if cname==None: cname==cmp.__name__
-    assert cmp(a,b),f'{cname}:\n{a}\n{b}'
+def test(a,b,cmp,cname=None):
+    if cname is None: cname=cmp.__name__
+    assert cmp(a,b),f"{cname}:\n{a}\n{b}"
 
-def test_eq(a, b):
-    test(a, b, operator.eq)
+def test_eq(a,b): test(a,b,operator.eq,'==')
 
-import matplotlib.pyplot as plt
-import gzip, math, torch, pickle, matplotlib as mpl
-from IPython.core.debugger import set_trace
-from torch import tensor, nn
 from pathlib import Path
-from torch.nn import init
+from IPython.core.debugger import set_trace
+import pickle, gzip, math, torch, matplotlib as mpl
+import matplotlib.pyplot as plt
+from torch import tensor
 
-mnist_path=Path(r'D:\git\dl\data\mnist.pkl.gz')
+mnist_path=Path('D:\git\dl\data\mnist.pkl.gz')
+Path.ls=lambda x: list(x.iterdir())
 
-def get_data(path):
-    with gzip.open(path, 'rb') as f:
-        ((x_train, y_train), (x_valid, y_valid), _)= pickle.load(f, encoding='latin_1')
-    return map(tensor, (x_train, y_train, x_valid, y_valid))
+def near(a,b): return torch.allclose(a, b, rtol=1e-3, atol=1e-5)
+def test_near(a,b): test(a,b,near)
 
 def normalize(x, m, s): return (x-m)/s
 
 def test_near_zero(a,tol=1e-3): assert a.abs()<tol, f"Near zero: {a}"
 
-def mse(output, targ): return (output.squeeze(-1) - targ).pow(2).mean()
-
+from torch.nn import init
+from torch import nn
 import torch.nn.functional as F
-
-def accuracy(out, yb): return (torch.argmax(out, dim=1)==yb).float().mean()
-
-class Dataset():
-    def __init__(self, x, y):
-        self.x, self.y= x, y
-    def __len__(self): return len(self.x)
-    def __getitem__(self, i): return self.x[i], self.y[i]
-
+from torch import optim
 from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
 
-def get_dls(train_ds, valid_ds, bs, **kwargs):
-    return (DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
-            DataLoader(valid_ds, batch_size=bs*2, **kwargs))
+
+def mse(output, targ): return (output.squeeze(-1) - targ).pow(2).mean()
+
+from typing import *
+from functools import partial
+
+def listify(o):
+    if o is None: return []
+    if isinstance(o, list): return o
+    if isinstance(o, str): return [o]
+    if isinstance(o, Iterable): return list(o)
+    return [o]
+
+class AvgStats():
+    def __init__(self, metrics, in_train): self.metrics,self.in_train = listify(metrics),in_train
+
+    def reset(self):
+        self.tot_loss,self.count = 0.,0
+        self.tot_mets = [0.] * len(self.metrics)
+
+    @property
+    def all_stats(self): return [self.tot_loss.item()] + self.tot_mets
+    @property
+    def avg_stats(self): return [o/self.count for o in self.all_stats]
+
+    def __repr__(self):
+        if not self.count: return ""
+        return f"{'train' if self.in_train else 'valid'}: {self.avg_stats}"
+
+    def accumulate(self, run):
+        bn = run.xb.shape[0]
+        self.tot_loss += run.loss * bn
+        self.count += bn
+        for i,m in enumerate(self.metrics):
+            self.tot_mets[i] += m(run.pred, run.yb) * bn
+
+import re
+
+_camel_re1 = re.compile('(.)([A-Z][a-z]+)')
+_camel_re2 = re.compile('([a-z0-9])([A-Z])')
+def camel2snake(name):
+    s1 = re.sub(_camel_re1, r'\1_\2', name)
+    return re.sub(_camel_re2, r'\1_\2', s1).lower()
+
+class Callback():
+    _order=0
+    def set_runner(self, run): self.run=run
+    def __getattr__(self, k): return getattr(self.run, k)
+    @property
+    def name(self):
+        name = re.sub(r'Callback$', '', self.__class__.__name__)
+        return camel2snake(name or 'callback')
+
+    def __call__(self, cb_name):
+        f = getattr(self, cb_name, None)
+        if f and f(): return True
+        return False
+
+class TrainEvalCallback(Callback):
+    def begin_fit(self):
+        self.run.n_epochs=0.
+        self.run.n_iter=0
+
+    def after_batch(self):
+        if not self.in_train: return
+        self.run.n_epochs += 1./self.iters
+        self.run.n_iter   += 1
+
+    def begin_epoch(self):
+        self.run.n_epochs=self.epoch
+        self.model.train()
+        self.run.in_train=True
+
+    def begin_validate(self):
+        self.model.eval()
+        self.run.in_train=False
+
+
+class CancelTrainException(Exception): pass
+class CancelEpochException(Exception): pass
+class CancelBatchException(Exception): pass
+
+class AvgStatsCallback(Callback):
+    def __init__(self, metrics):
+        self.train_stats,self.valid_stats = AvgStats(metrics,True),AvgStats(metrics,False)
+
+    def begin_epoch(self):
+        self.train_stats.reset()
+        self.valid_stats.reset()
+
+    def after_loss(self):
+        stats = self.train_stats if self.in_train else self.valid_stats
+        with torch.no_grad(): stats.accumulate(self.run)
+
+    def after_epoch(self):
+        print(self.train_stats)
+        print(self.valid_stats)
+
+def annealer(f):
+    def _inner(start, end): return partial(f, start, end)
+    return _inner
+
+@annealer
+def sched_lin(start, end, pos): return start + pos*(end-start)
+
+@annealer
+def sched_cos(start, end, pos): return start + (1 + math.cos(math.pi*(1-pos))) * (end-start) / 2
+@annealer
+def sched_no(start, end, pos):  return start
+@annealer
+def sched_exp(start, end, pos): return start * (end/start) ** pos
+
+torch.Tensor.ndim = property(lambda x: len(x.shape))
+
+def combine_scheds(pcts, scheds):
+    assert sum(pcts) == 1.
+    pcts = tensor([0] + listify(pcts))
+    assert torch.all(pcts >= 0)
+    pcts = torch.cumsum(pcts, 0)
+    def _inner(pos):
+        idx = (pos >= pcts).nonzero().max()
+        actual_pos = (pos-pcts[idx]) / (pcts[idx+1]-pcts[idx])
+        return scheds[idx](actual_pos)
+    return _inner
+
+class Recorder(Callback):
+    def begin_fit(self): self.lrs,self.losses = [],[]
+
+    def after_batch(self):
+        if not self.in_train: return
+        self.lrs.append(self.opt.hypers[-1]['lr'])
+        self.losses.append(self.loss.detach().cpu())
+
+    def plot_lr  (self): plt.plot(self.lrs)
+    def plot_loss(self): plt.plot(self.losses)
+
+    def plot(self, skip_last=0):
+        losses = [o.item() for o in self.losses]
+        n = len(losses)-skip_last
+        plt.xscale('log')
+        plt.plot(self.lrs[:n], losses[:n])
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs):
+        self.pname,self.sched_funcs = pname,listify(sched_funcs)
+
+    def begin_batch(self):
+        if not self.in_train: return
+        fs = self.sched_funcs
+        if len(fs)==1: fs = fs*len(self.opt.param_groups)
+        pos = self.n_epochs/self.epochs
+        for f,h in zip(fs,self.opt.hypers): h[self.pname] = f(pos)
+
+class LR_Find(Callback):
+    _order=1
+    def __init__(self, max_iter=100, min_lr=1e-6, max_lr=10):
+        self.max_iter,self.min_lr,self.max_lr = max_iter,min_lr,max_lr
+        self.best_loss = 1e9
+
+    def begin_batch(self):
+        if not self.in_train: return
+        pos = self.n_iter/self.max_iter
+        lr = self.min_lr * (self.max_lr/self.min_lr) ** pos
+        for pg in self.opt.hypers: pg['lr'] = lr
+
+    def after_step(self):
+        if self.n_iter>=self.max_iter or self.loss>self.best_loss*10:
+            raise CancelTrainException()
+        if self.loss < self.best_loss: self.best_loss = self.loss
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs): self.pname,self.sched_funcs = pname,sched_funcs
+
+    def begin_fit(self):
+        if not isinstance(self.sched_funcs, (list,tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+
+    def set_param(self):
+        assert len(self.opt.param_groups)==len(self.sched_funcs)
+        for pg,f in zip(self.opt.param_groups,self.sched_funcs):
+            pg[self.pname] = f(self.n_epochs/self.epochs)
+
+    def begin_batch(self):
+        if self.in_train: self.set_param()
+
+def pg_dicts(pgs): return [{'params':o} for o in pgs]
+
+def cos_1cycle_anneal(start, high, end):
+    return [sched_cos(start, high), sched_cos(high, end)]
+
+def maybe_update(os, dest, f):
+    for o in os:
+        for k,v in f(o).items():
+            if k not in dest: dest[k] = v
+
+def get_defaults(d): return getattr(d,'_defaults',{})
+
+def sgd_step(p, lr, **kwargs):
+    p.data.add_(-lr, p.grad.data)
+    return p
+
+def weight_decay(p, lr, wd, **kwargs):
+    p.data.mul_(1 - lr*wd)
+    return p
+weight_decay._defaults = dict(wd=0.)
+
+def l2_reg(p, lr, wd, **kwargs):
+    p.grad.data.add_(wd, p.data)
+    return p
+l2_reg._defaults = dict(wd=0.)
+
+def param_getter(m): return m.parameters()
+
+def compose(x, funcs, *args, order_key='_order', **kwargs):
+    key = lambda o: getattr(o, order_key, 0)
+    for f in sorted(listify(funcs), key=key): x = f(x, **kwargs)
+    return x
+
+class Optimizer():
+    def __init__(self, params, steppers, **defaults):
+        self.steppers = listify(steppers)
+        maybe_update(self.steppers, defaults, get_defaults)
+        # might be a generator
+        self.param_groups = list(params)
+        # ensure params is a list of lists
+        if not isinstance(self.param_groups[0], list): self.param_groups = [self.param_groups]
+        self.hypers = [{**defaults} for p in self.param_groups]
+
+    def grad_params(self):
+        return [(p,hyper) for pg,hyper in zip(self.param_groups,self.hypers)
+            for p in pg if p.grad is not None]
+
+    def zero_grad(self):
+        for p,hyper in self.grad_params():
+            p.grad.detach_()
+            p.grad.zero_()
+
+    def step(self):
+        for p,hyper in self.grad_params(): compose(p, self.steppers, **hyper)
+
+sgd_opt = partial(Optimizer, steppers=[weight_decay, sgd_step])
+
+class Learner():
+    def __init__(self, model, data, loss_func, opt_func=sgd_opt, lr=1e-2, splitter=param_getter,
+                 cbs=None, cb_funcs=None):
+        self.model,self.data,self.loss_func,self.opt_func,self.lr,self.splitter = model,data,loss_func,opt_func,lr,splitter
+        self.in_train,self.logger,self.opt = False,print,None
+
+        # NB: Things marked "NEW" are covered in lesson 12
+        # NEW: avoid need for set_runner
+        self.cbs = []
+        self.add_cb(TrainEvalCallback())
+        self.add_cbs(cbs)
+        self.add_cbs(cbf() for cbf in listify(cb_funcs))
+
+    def add_cbs(self, cbs):
+        for cb in listify(cbs): self.add_cb(cb)
+
+    def add_cb(self, cb):
+        cb.set_runner(self)
+        setattr(self, cb.name, cb)
+        self.cbs.append(cb)
+
+    def remove_cbs(self, cbs):
+        for cb in listify(cbs): self.cbs.remove(cb)
+
+    def one_batch(self, i, xb, yb):
+        try:
+            self.iter = i
+            self.xb,self.yb = xb,yb;                        self('begin_batch')
+            self.pred = self.model(self.xb);                self('after_pred')
+            self.loss = self.loss_func(self.pred, self.yb); self('after_loss')
+            if not self.in_train: return
+            self.loss.backward();                           self('after_backward')
+            self.opt.step();                                self('after_step')
+            self.opt.zero_grad()
+        except CancelBatchException:                        self('after_cancel_batch')
+        finally:                                            self('after_batch')
+
+    def all_batches(self):
+        self.iters = len(self.dl)
+        try:
+            for i,(xb,yb) in enumerate(self.dl): self.one_batch(i, xb, yb)
+        except CancelEpochException: self('after_cancel_epoch')
+
+    def do_begin_fit(self, epochs):
+        self.epochs,self.loss = epochs,tensor(0.)
+        self('begin_fit')
+
+    def do_begin_epoch(self, epoch):
+        self.epoch,self.dl = epoch,self.data.train_dl
+        return self('begin_epoch')
+
+    def fit(self, epochs, cbs=None, reset_opt=False):
+        # NEW: pass callbacks to fit() and have them removed when done
+        self.add_cbs(cbs)
+        # NEW: create optimizer on fit(), optionally replacing existing
+        if reset_opt or not self.opt: self.opt = self.opt_func(self.splitter(self.model), lr=self.lr)
+
+        try:
+            self.do_begin_fit(epochs)
+            for epoch in range(epochs):
+                if not self.do_begin_epoch(epoch): self.all_batches()
+
+                with torch.no_grad():
+                    self.dl = self.data.valid_dl
+                    if not self('begin_validate'): self.all_batches()
+                self('after_epoch')
+
+        except CancelTrainException: self('after_cancel_train')
+        finally:
+            self('after_fit')
+            self.remove_cbs(cbs)
+
+    ALL_CBS = {'begin_batch', 'after_pred', 'after_loss', 'after_backward', 'after_step',
+        'after_cancel_batch', 'after_batch', 'after_cancel_epoch', 'begin_fit',
+        'begin_epoch', 'begin_validate', 'after_epoch',
+        'after_cancel_train', 'after_fit'}
+
+    def __call__(self, cb_name):
+        res = False
+        assert cb_name in self.ALL_CBS
+        for cb in sorted(self.cbs, key=lambda x: x._order): res = cb(cb_name) and res
+        return res
+
+def normalize_to(train, valid):
+    m,s = train.mean(),train.std()
+    return normalize(train, m, s), normalize(valid, m, s)
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x): return self.func(x)
+
+def flatten(x):      return x.view(x.shape[0], -1)
+
+class CudaCallback(Callback):
+    def begin_fit(self): self.model.cuda()
+    def begin_batch(self): self.run.xb,self.run.yb = self.xb.cuda(),self.yb.cuda()
+
+class BatchTransformXCallback(Callback):
+    _order=2
+    def __init__(self, tfm): self.tfm = tfm
+    def begin_batch(self): self.run.xb = self.tfm(self.xb)
+
+def view_tfm(*size):
+    def _inner(x): return x.view(*((-1,)+size))
+    return _inner
+
+def get_cnn_layers(data, nfs, layer, **kwargs):
+    nfs = [1] + nfs
+    return [layer(nfs[i], nfs[i+1], 5 if i==0 else 3, **kwargs)
+            for i in range(len(nfs)-1)] + [
+        nn.AdaptiveAvgPool2d(1), Lambda(flatten), nn.Linear(nfs[-1], data.c)]
+
+def conv_layer(ni, nf, ks=3, stride=2, **kwargs):
+    return nn.Sequential(
+        nn.Conv2d(ni, nf, ks, padding=ks//2, stride=stride), GeneralRelu(**kwargs))
+
+class GeneralRelu(nn.Module):
+    def __init__(self, leak=None, sub=None, maxv=None):
+        super().__init__()
+        self.leak,self.sub,self.maxv = leak,sub,maxv
+
+    def forward(self, x):
+        x = F.leaky_relu(x,self.leak) if self.leak is not None else F.relu(x)
+        if self.sub is not None: x.sub_(self.sub)
+        if self.maxv is not None: x.clamp_max_(self.maxv)
+        return x
+
+def init_cnn(m, uniform=False):
+    f = init.kaiming_uniform_ if uniform else init.kaiming_normal_
+    for l in m:
+        if isinstance(l, nn.Sequential):
+            f(l[0].weight, a=0.1)
+            l[0].bias.data.zero_()
+
+def get_cnn_model(data, nfs, layer, **kwargs):
+    return nn.Sequential(*get_cnn_layers(data, nfs, layer, **kwargs))
+
+def children(m): return list(m.children())
+
+class Hook():
+    def __init__(self, m, f): self.hook = m.register_forward_hook(partial(f, self))
+    def remove(self): self.hook.remove()
+    def __del__(self): self.remove()
+
+def append_stats(hook, mod, inp, outp):
+    if not hasattr(hook,'stats'): hook.stats = ([],[])
+    means,stds = hook.stats
+    means.append(outp.data.mean())
+    stds .append(outp.data.std())
+
+class ProgressCallback(Callback):
+    _order=-1
+    def begin_fit(self):
+        self.mbar = master_bar(range(self.epochs))
+        self.mbar.on_iter_begin()
+        self.run.logger = partial(self.mbar.write, table=True)
+
+    def after_fit(self): self.mbar.on_iter_end()
+    def after_batch(self): self.pb.update(self.iter)
+    def begin_epoch   (self): self.set_pb()
+    def begin_validate(self): self.set_pb()
+
+    def set_pb(self):
+        self.pb = progress_bar(self.dl, parent=self.mbar, auto_update=False)
+        self.mbar.update(self.epoch)
+
+import time
+from fastprogress import master_bar, progress_bar
+from fastprogress.fastprogress import format_time
+
+class AvgStatsCallback(Callback):
+    def __init__(self, metrics):
+        self.train_stats,self.valid_stats = AvgStats(metrics,True),AvgStats(metrics,False)
+
+    def begin_fit(self):
+        met_names = ['loss'] + [m.__name__ for m in self.train_stats.metrics]
+        names = ['epoch'] + [f'train_{n}' for n in met_names] + [
+            f'valid_{n}' for n in met_names] + ['time']
+        self.logger(names)
+
+    def begin_epoch(self):
+        self.train_stats.reset()
+        self.valid_stats.reset()
+        self.start_time = time.time()
+
+    def after_loss(self):
+        stats = self.train_stats if self.in_train else self.valid_stats
+        with torch.no_grad(): stats.accumulate(self.run)
+
+    def after_epoch(self):
+        stats = [str(self.epoch)]
+        for o in [self.train_stats, self.valid_stats]:
+            stats += [f'{v:.6f}' for v in o.avg_stats]
+        stats += [format_time(time.time() - self.start_time)]
+        self.logger(stats)
